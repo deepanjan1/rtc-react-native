f07b136202c04d25948bf6af53d74a706c42e519
deepanjan1 <deepanjan.de@gmail.com>
2018-04-12 16:31:09 -0400

    Remove duplicated directory

--- src/config/apiconfig.js
+++
@@ -1,8 +0,0 @@
-
-//Firebase API Information
-export const apiKey = 'AIzaSyDO4KOLYpxKTEVQ_Fp1Qx2aLCrgs4KXdvc';
-export const authDomain = 'rtc-react-native.firebaseapp.com';
-export const databaseURL = 'https://rtc-react-native.firebaseio.com';
-export const projectId = 'rtc-react-native';
-export const storageBucket = 'rtc-react-native.appspot.com';
-export const messagingSenderId = '819008592100';
